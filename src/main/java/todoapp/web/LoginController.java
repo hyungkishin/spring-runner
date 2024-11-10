7f2cc0ee4596231cb32ff98d6283c357254066ae
hyungkishin <tkaqkeldk@nate.com>
2024-11-10 15:53:02 +0900

    feat: login 기능 완성password 가 틀렸을때, ExceptionHandler 어노테이션을 이용하는 방법이 존재하나 - 하나의 endpoint 에서 비즈니스 로직이 모두 보이는 방법으로 채택

--- src/main/java/todoapp/web/LoginController.java
+++ src/main/java/todoapp/web/LoginController.java
@@ -1,17 +1,71 @@
 package todoapp.web;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import todoapp.core.user.application.UserPasswordVerifier;
+import todoapp.core.user.application.UserRegistration;
+import todoapp.core.user.domain.User;
+import todoapp.core.user.domain.UserEntityNotFoundException;
+import todoapp.core.user.domain.UserPasswordNotMatchedException;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
 
+    private final UserPasswordVerifier userPasswordVerifier;
+
+    private final UserRegistration userRegistration;
+
     @RequestMapping("/login")
     public void loginForm(Model model) {
 
+    }
+
+    @PostMapping("/login")
+    public String loginProcess(
+            @Valid final LoginCommand request,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        log.debug("bindingResult: {}", bindingResult);
+
+        // 0. 입력 값 점증에 실패한 경우: login 페이지로 돌려보내고, 오류 메시지 노출하기
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("bindingResult", bindingResult);
+            model.addAttribute("message", "입력 값에 문제가 있어요");
+            return "login";
+        }
+        try {
+            User user = userPasswordVerifier.verify(request.username, request.password);
+            user.verifyPassword(request.password);
+        } catch (UserEntityNotFoundException e) {
+            userRegistration.join(request.username(), request.password());
+        } catch (UserPasswordNotMatchedException e) {
+            model.addAttribute("bindingResult", bindingResult);
+            model.addAttribute("message", "비밀번호 틀렸습니다.");
+            return "login";
+        }
+
+        return "redirect:/todos";
+    }
+
+    /* 나의 경우에는 한눈에 보이는게 좋을것 같아서, 따로 두진 않고싶어.
+    @ExceptionHandler(UserPasswordNotMatchedException.class)
+        public String handleUserPasswordInvalid(UserPasswordNotMatchedException e ,Model model) {
+            model.addAttribute("message", e.getMessage());
+            return "login";
+    }
+    */
+
+    record LoginCommand(@Size(min = 4, max = 20) String username, String password) {
     }
 
 }
