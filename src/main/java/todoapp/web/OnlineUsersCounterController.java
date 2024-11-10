eeb4e7b0245eadb31c1e0dc2c9b2e483a951e873
hyungkishin <tkaqkeldk@nate.com>
2024-11-10 14:07:30 +0900

    feat: 실시간 사이트 접속 사용자 수 카운터 컨트롤러를 세부적으로 구현 - 비동기

--- src/main/java/todoapp/web/OnlineUsersCounterController.java
+++ src/main/java/todoapp/web/OnlineUsersCounterController.java
@@ -3,9 +3,8 @@ package todoapp.web;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import todoapp.web.support.ConnectedClientCountBroadcaster;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * 실시간 사이트 접속 사용자 수 카운터 컨트롤러이다.
@@ -20,7 +19,7 @@ public class OnlineUsersCounterController {
     /*
      * HTML5 Server-sent events(https://en.wikipedia.org/wiki/Server-sent_events) 명세를 구현했다.
      */
-    @RequestMapping(path = "/stream/online-users-counter", produces = "text/event-stream")
+    /*@RequestMapping(path = "/stream/online-users-counter", produces = "text/event-stream")
     public void counter(HttpServletResponse response) throws Exception {
         response.setCharacterEncoding("utf-8");
         response.setContentType("text/event-stream");
@@ -32,6 +31,9 @@ public class OnlineUsersCounterController {
             outputStream.write(("data: " + number + "\n\n").getBytes());
             outputStream.flush();
         }
+    }*/
+    @RequestMapping(path = "/stream/online-users-counter", produces = "text/event-stream")
+    public SseEmitter counter(HttpServletResponse response) throws Exception {
+        return broadcaster.subscribe();
     }
-
 }
